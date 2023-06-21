a6a9c6663d40913a6168f059f8d8b65fcc934a4c
iziz9 <46iz.us2@gmail.com>
2023-06-21 16:56:57 +0900

    Env: 스타일변수 객체로 분리

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,57 @@
 import styled from 'styled-components'
+import { Link, useNavigate } from 'react-router-dom'
+import { COLORS } from '@src/globalStyles'
 
 const Header = () => {
-  return <Container>header</Container>
+  // 모바일버전에는 없음
+  // pc, tab 폰트사이즈 확인
+
+  const navigate = useNavigate()
+
+  return (
+    <Container>
+      <Inner>
+        <Link to="/" className="logo">
+          <img src="/logo.png" alt="필드패서" />
+        </Link>
+        <div className="menu">
+          <Link to="/">회원가입</Link>
+          <Link to="/">로그인</Link>
+          <button>양도하기</button>
+        </div>
+      </Inner>
+    </Container>
+  )
 }
 
-const Container = styled.p`
-  color: #ddd;
+const Container = styled.header`
+  padding: 12px 20px;
+  height: 60px;
+  box-sizing: border-box;
+`
+const Inner = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  .logo {
+    img {
+      width: 160px;
+      height: 24px;
+    }
+  }
+
+  .menu {
+    display: flex;
+    gap: 20px;
+    align-items: center;
+
+    button {
+      width: 100px;
+      height: 32px;
+      background-color: ${COLORS.green};
+      color: white;
+    }
+  }
 `
 
 export default Header
