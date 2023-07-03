064ad1965ee082abb58c19abdb1a1f1159a8ad3f
HyunSooBae <hyunsoobae.dev@gmail.com>
2023-07-04 03:54:59 +0900

    Feat: Cookie.jsx 파일 생성하고 쿠키 관련 함수 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from '@src/routes/router'
 import GlobalStyles from './globalStyles'
+import { CookiesProvider } from 'react-cookie'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-    <GlobalStyles />
-    <RouterProvider router={router} />
-  </Provider>
+  <CookiesProvider>
+    <Provider store={store}>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </Provider>
+  </CookiesProvider>
 )
