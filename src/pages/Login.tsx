f011227fc4a465dcad59d29ff8020b769f99bd8c
HyunSooBae <hyunsoobae.dev@gmail.com>
2023-07-19 19:50:36 +0900

    Comment: Login.tsx 주석 제거

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,10 +8,6 @@ import { useDispatch } from 'react-redux'
 import { removeCookieToken, setRefreshToken } from '@src/storage/Cookie'
 import { userLogin } from '@src/api/authApi'
 
-// 첫 로그인 요청은 id(email), pw 필요
-// 새 at 재발급 요청 시 at, rt 둘다 필요 (at 만료되서 UNAUTHORIZED(401) 돌아오면)
-// 로그아웃 요청 시 유효한 at 필요 (rt는 필요없음 쿠키스토리지에서 삭제만ㄱ)
-
 const Login = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
@@ -29,7 +25,8 @@ const Login = () => {
     })
   }
 
-  const onSubmitHandler = async () => {
+  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
     const { status, tokens } = await userLogin({
       userEmail,
       userPw,
@@ -38,38 +35,30 @@ const Login = () => {
       removeCookieToken()
       dispatch(SET_TOKEN(tokens.accessToken))
       setRefreshToken(tokens.refreshToken)
-      // console.log('로그인함', new Date())
+      console.log('로그인함', new Date())
       return navigate('/')
     }
   }
 
   return (
     <Container>
-      <div className="logo">{/* <img src="/logo.png" alt="필드패서" /> */}</div>
-
       <form onSubmit={onSubmitHandler}>
+        <div className="logo">
+          <img src="/logo.png" alt="필드패서" />
+        </div>
+
         <div className="input_wrap">
           <label>이메일</label>
-          <input
-            type="text"
-            name="userEmail"
-            //pattern="^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-            placeholder="이메일을 적어주세요"
-            title="올바른 형식의 이메일을 적어주세요"
-            onChange={onChangeHandler}
-            //required
-          />
+          <input type="text" name="userEmail" placeholder="이메일을 적어주세요" title="올바른 형식의 이메일을 적어주세요" onChange={onChangeHandler} required />
+
           <label>비밀번호</label>
           <input
             type="password"
             name="userPw"
-            //minLength={8}
-            //maxLength={16}
-            //pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$"
             placeholder="비밀번호를 적어주세요"
             title="최소 8자리, 최대 16자리 숫자, 영문, 특수문자를 1개 이상 포함해주세요"
             onChange={onChangeHandler}
-            //required
+            required
           />
         </div>
 
