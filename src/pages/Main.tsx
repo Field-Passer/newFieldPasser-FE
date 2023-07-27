7b4919bbc403a3498dc707570f7703bd1b355645
iziz9 <46iz.us2@gmail.com>
2023-07-27 19:34:42 +0900

    Design: 메인페이지 배너 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -9,10 +9,13 @@ import Board from '@src/components/Board'
 import { getSearchPostList } from '@src/api/getApi'
 
 const Main = () => {
-  const [isActive, setIsActive] = useState<boolean>(false)
+  const isMobile = useMediaQuery({
+    query: '(max-width: 833px)',
+  })
   const [postList, setPostList] = useState<POST_TYPE[]>([])
   const [district, setDistrict] = useState<string>('')
   const [category, setCategory] = useState<string>('')
+  const [background, setBackground] = useState<string>('')
   const searchValue = {
     title: '',
     startTime: '',
@@ -21,12 +24,20 @@ const Main = () => {
     category: category,
     date: '',
   }
-
-  const isMobile = useMediaQuery({
-    query: '(max-width: 833px)',
+  const [isActive, setIsActive] = useState({
+    futsal: true,
+    soccer: false,
+    basketball: false,
+    badminton: false,
+    tennis: false,
   })
 
   useEffect(() => {
+    setBackground(`/banner${Math.floor(Math.random() * 5)}.png`)
+  }, [])
+
+  useEffect(() => {
+    // api재요청 / 무한스크롤이면?
     const getPostList = async () => {
       try {
         const postData = await getSearchPostList(searchValue)
@@ -42,35 +53,101 @@ const Main = () => {
   const categories: ICategories[] = [
     {
       category: 'futsal',
-      name: '풋살장',
-      icon: <FutsalIcon color={isActive ? COLORS.green : '#00000099'} />,
+      name: '풋살',
+      icon: <FutsalIcon color={isActive.futsal ? COLORS.green : '#00000099'} />,
     },
     {
       category: 'soccer',
-      name: '축구장',
-      icon: <SoccerIcon color={isActive ? COLORS.green : '#00000099'} />,
+      name: '축구',
+      icon: <SoccerIcon color={isActive.soccer ? COLORS.green : '#00000099'} />,
     },
     {
       category: 'basketball',
-      name: '농구장',
-      icon: <BasketballIcon color={isActive ? COLORS.green : '#00000099'} />,
+      name: '농구',
+      icon: <BasketballIcon color={isActive.basketball ? COLORS.green : '#00000099'} />,
     },
     {
       category: 'badminton',
-      name: '배드민턴장',
-      icon: <BadmintonIcon color={isActive ? COLORS.green : '#00000099'} />,
+      name: '배드민턴',
+      icon: <BadmintonIcon color={isActive.badminton ? COLORS.green : '#00000099'} />,
     },
     {
       category: 'tennis',
-      name: '테니스장',
-      icon: <TennisIcon color={isActive ? COLORS.green : '#00000099'} />,
+      name: '테니스',
+      icon: <TennisIcon color={isActive.tennis ? COLORS.green : '#00000099'} />,
     },
   ]
 
   const sortOptions = ['가장 최신 순', '인기순', '낮은 가격 순', '높은 가격 순']
 
+  const handleClickCategory = (category: string) => {
+    switch (category) {
+      case 'futsal':
+        setIsActive({
+          futsal: true,
+          soccer: false,
+          basketball: false,
+          badminton: false,
+          tennis: false,
+        })
+        setCategory('풋살장')
+        break
+      case 'soccer':
+        setIsActive({
+          futsal: false,
+          soccer: true,
+          basketball: false,
+          badminton: false,
+          tennis: false,
+        })
+        setCategory('축구장')
+        break
+      case 'basketball':
+        setIsActive({
+          futsal: false,
+          soccer: false,
+          basketball: true,
+          badminton: false,
+          tennis: false,
+        })
+        setCategory('농구장')
+        break
+      case 'badminton':
+        setIsActive({
+          futsal: false,
+          soccer: false,
+          basketball: false,
+          badminton: true,
+          tennis: false,
+        })
+        setCategory('배드민턴장')
+        break
+      case 'tennis':
+        setIsActive({
+          futsal: false,
+          soccer: false,
+          basketball: false,
+          badminton: false,
+          tennis: true,
+        })
+        setCategory('테니스장')
+        break
+    }
+  }
+
   return (
     <Container>
+      {!isMobile && (
+        <section className="banner-section" style={{ backgroundImage: `url(${background})` }}>
+          <div className="text">
+            <div className="first">
+              <span>아까운</span> <span>양도수수료</span>
+            </div>
+            <div className="second">이제 버리지 말고 양도 하세요!</div>
+            <div className="third">간단한 가입절차와 상세한 게시글 필터 기능으로 간편하게 체육시설을 양도/양수해보세요!</div>
+          </div>
+        </section>
+      )}
       <section className="search-section">
         <SearchForm />
       </section>
@@ -78,9 +155,15 @@ const Main = () => {
         <Category className={isMobile ? 'mobile' : 'pc'}>
           {categories.map((item) => {
             return (
-              <div key={item.category} className="icon">
+              <div
+                key={item.category}
+                className="icon"
+                onClick={() => {
+                  handleClickCategory(item.category)
+                }}
+              >
                 {item.icon}
-                <span>{item.name}</span>
+                <span className={isActive[item.category] ? 'green' : ''}>{item.name}</span>
               </div>
             )
           })}
@@ -121,7 +204,16 @@ const Container = styled.main`
   display: block;
   font-size: ${FONT.m};
   margin: auto;
-  max-width: 834px;
+  max-width: 1024px;
+
+  .banner-section {
+    width: 100%;
+    height: 312px;
+    max-width: 1024px;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
 
   .search-section {
     margin: auto;
@@ -133,7 +225,6 @@ const ListSection = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
-  padding: 0 16px;
 `
 const Category = styled.div`
   height: 70px;
@@ -141,6 +232,10 @@ const Category = styled.div`
   border-bottom: 1px solid ${COLORS.gray20};
   margin: auto;
   gap: 16px;
+
+  @media (max-width: 599px) {
+    width: 100%;
+  }
 
   @media (min-width: 600px) {
     gap: 28px;
@@ -177,12 +272,17 @@ const Category = styled.div`
         height: 32px;
       }
     }
+
+    .green {
+      color: ${COLORS.green};
+    }
   }
 `
 
 const Options = styled.div`
   display: flex;
   gap: 14px;
+  padding: 0 16px;
 
   select {
     width: 100px;
