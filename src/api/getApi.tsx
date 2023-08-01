61d64c0ec53f4e73578978359f9ff1f76100d3b7
0nesan <0nesan.o8o@gmail.com>
2023-08-01 20:23:28 +0900

    feat: 게시글 상세 api연동

--- src/api/getApi.tsx
+++ src/api/getApi.tsx
@@ -1,18 +1,30 @@
-import { publicApi } from './Instance'
+import { publicApi, privateApi } from './Instance'
 
 export const getSearchPostList = async (values: SearchValueTypes, page = 1) => {
   if (values.startTime.slice(0, 10) === values.endTime.slice(0, 10)) {
     values.startTime = ''
     values.endTime = ''
   }
-  console.log(values)
 
   return await publicApi
     .get(
-      `/search/${page}?title=${values.title}&categoryName=${values.category}&startTime=${values.startTime}&endTime=${values.endTime
+      `/search/${page}?title=${values.title}&categoryName=${values.category}&startTime=${values.startTime}&endTime=${
+        values.endTime
       }&districtNames=${values.district.join()}`
     )
     .then((res) => {
+      return res.data.data
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+export const getPostDetail = async (userId: number) => {
+  return await privateApi
+    .get(`/board/${userId}`)
+    .then((res) => {
+      console.log(res.data.data)
       return res.data.data
     })
     .catch((err) => {
