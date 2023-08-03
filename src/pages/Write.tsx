de48a892b4d6c787ee7cc910927e975cc766020e
iziz9 <46iz.us2@gmail.com>
2023-08-03 18:47:29 +0900

    Fix: 사이드바 실행 코드 수정

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -479,6 +479,7 @@ const Container = styled.main`
   textarea {
     &:focus {
       outline: none;
+      background-color: none;
     }
   }
 
@@ -559,6 +560,26 @@ const Container = styled.main`
 
   .react-datepicker__day--keyboard-selected {
     background: none;
+  }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active {
+    -webkit-text-fill-color: ${COLORS.font};
+    -webkit-box-shadow: 0 0 0px 1000px #fff inset;
+    box-shadow: 0 0 0px 1000px #fff inset;
+    transition: background-color 5000s ease-in-out 0s;
+  }
+
+  input:autofill,
+  input:autofill:hover,
+  input:autofill:focus,
+  input:autofill:active {
+    -webkit-text-fill-color: ${COLORS.font};
+    -webkit-box-shadow: 0 0 0px 1000px #fff inset;
+    box-shadow: 0 0 0px 1000px #fff inset;
+    transition: background-color 5000s ease-in-out 0s;
   }
 `
 
