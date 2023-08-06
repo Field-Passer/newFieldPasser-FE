12e2b0bd2e94641aedf8e018aec4c5327be5fcd0
quokka-eating-carrots <ovzip7@gmail.com>
2023-08-06 19:02:56 +0900

    Feat: 마이 페이지 상세 api 연결

--- src/store/slices/wishlistSlice.tsx
+++ src/store/slices/wishlistSlice.tsx
@@ -1,4 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+const storage = JSON.parse(localStorage.getItem('persist:root') as string)
+const token = JSON.parse(storage.accessToken)
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  prepareHeaders: (headers) => {
+    headers.set('Authorization', `Bearer ${token.accessToken}`)
+    return headers
+  },
+})
+
+export const wishlistApi = createApi({
+  reducerPath: 'wishlistApi',
+  baseQuery,
+  tagTypes: ['Wishlist'],
+  endpoints: (builder) => ({
+    getWishlist: builder.query({
+      query: (page: number = 1) => `/my-page/wish-list/${page}`,
+      providesTags: [{ type: 'Wishlist', id: 'WISHLIST' }],
+    }),
+  }),
+})
 
 export const wishlistSlice = createSlice({
   name: 'wishlist',
@@ -13,4 +38,5 @@ export const wishlistSlice = createSlice({
 })
 
 export const { SET_WISHLIST } = wishlistSlice.actions
+export const { useGetWishlistQuery } = wishlistApi
 export default wishlistSlice
