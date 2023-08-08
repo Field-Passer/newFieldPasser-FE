dd1335437927e6b62dd6edf6a8f1c1da89ddc635
quokka-eating-carrots <ovzip7@gmail.com>
2023-08-08 17:15:09 +0900

    Feat: 1:1 문의 모바일 마크업

--- src/pages/HelpForm.tsx
+++ src/pages/HelpForm.tsx
@@ -14,20 +14,31 @@ const HelpForm = () => {
         <Inner padding="64px 0">
           <Title screen="pc" name="1:1 문의하기" />
           <FormStyle>
-            <FormDetailStyle>
+            <FormDetailStyle screen="pc">
               <h2>제목</h2>
               <input type="text" />
             </FormDetailStyle>
-            <FormDetailStyle>
+            <FormDetailStyle screen="pc">
               <h2>내용</h2>
               <textarea cols={30} rows={10}></textarea>
             </FormDetailStyle>
+            <ButtonStyle screen="pc">문의 등록</ButtonStyle>
           </FormStyle>
-          <ButtonStyle>문의 등록</ButtonStyle>
         </Inner>
       ) : (
-        <Inner>
-          <Title screen="mobile" name="1:1 문의하기"></Title>
+        <Inner width="100%" padding="16px 0">
+          <Title screen="mobile" name="1:1 문의하기" />
+          <FormStyle>
+            <FormDetailStyle screen="mobile">
+              <h2>제목</h2>
+              <input type="text" />
+            </FormDetailStyle>
+            <FormDetailStyle screen="mobile">
+              <h2>내용</h2>
+              <textarea cols={30} rows={10}></textarea>
+            </FormDetailStyle>
+            <ButtonStyle screen="mobile">문의 등록</ButtonStyle>
+          </FormStyle>
         </Inner>
       )}
     </>
@@ -36,31 +47,35 @@ const HelpForm = () => {
 
 export default HelpForm
 
-const FormStyle = styled.div`
+interface StyleProps {
+  screen: string
+}
+
+const FormStyle = styled.form`
   display: flex;
   flex-direction: column;
   padding: 16px;
   gap: 32px;
 `
 
-const FormDetailStyle = styled.div`
+const FormDetailStyle = styled.div<StyleProps>`
   display: flex;
   flex-direction: column;
   gap: 8px;
   h2 {
-    font-size: ${FONT['pc-lg']};
+    font-size: ${({ screen }) => (screen === 'pc' ? FONT['pc-lg'] : FONT.m)};
   }
   input {
     padding: 16px 8px;
     border-radius: 8px;
     border: 1px solid ${COLORS.gray30};
-    font-size: ${FONT.pc};
+    font-size: ${({ screen }) => (screen === 'pc' ? FONT.pc : FONT['m-sm'])};
   }
   textarea {
     padding: 16px 8px;
     border-radius: 8px;
     border: 1px solid ${COLORS.gray30};
-    font-size: ${FONT.pc};
+    font-size: ${({ screen }) => (screen === 'pc' ? FONT.pc : FONT['m-sm'])};
     &:focus {
       border: 1px solid ${COLORS.gray30};
       outline: none;
@@ -68,13 +83,13 @@ const FormDetailStyle = styled.div`
   }
 `
 
-const ButtonStyle = styled.div`
+const ButtonStyle = styled.div<StyleProps>`
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: ${COLORS.green};
   color: #fff;
-  width: 328px;
+  width: ${({ screen }) => (screen === 'pc' ? '486px' : '100%')};
   height: 48px;
   font-size: ${FONT['pc-lg']};
   margin: 46px auto;
