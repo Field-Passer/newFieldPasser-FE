6804465a2e00b8f05c663e1d015303c73ff85c7b
HyunSooBae <hyunsoobae.dev@gmail.com>
2023-08-18 14:00:43 +0900

    Feat: 리덕스로 저장하던 액세스토큰 수정 - 리덕스로 한번에 관리하던 것을 직접 로컬스토리지에 저장하도록 수정함

--- src/store/slices/authSlice.tsx
+++ src/store/slices/authSlice.tsx
@@ -1,24 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-export const TOKEN_TIME_OUT = 10 * 1000 // 10초
+export const TOKEN_TIME_OUT = 10 * 1000 // 10초 // 테스트 할 때만 사용
 
 export const authSlice = createSlice({
   name: 'authToken',
   initialState: {
     authenticated: false,
-    accessToken: null,
     expireTime: 0,
   },
   reducers: {
     SET_TOKEN: (state, action) => {
       state.authenticated = true
-      state.accessToken = action.payload
       state.expireTime = new Date().getTime() + TOKEN_TIME_OUT
+      window.localStorage.setItem('accessToken', action.payload)
     },
     DELETE_TOKEN: (state) => {
       state.authenticated = false
-      state.accessToken = null
       state.expireTime = 0
+      window.localStorage.removeItem('accessToken')
     },
   },
 })
