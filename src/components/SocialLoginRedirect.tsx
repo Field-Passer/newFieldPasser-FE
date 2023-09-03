9d22c4b9e5d732d0c894ca4f53463872c1ea59dc
HyunSooBae <hyunsoobae.dev@gmail.com>
2023-09-03 18:54:00 +0900

    Comment: 콘솔로그 수정 및 제거

--- src/components/SocialLoginRedirect.tsx
+++ src/components/SocialLoginRedirect.tsx
@@ -17,13 +17,6 @@ const SocialLoginRedirect = () => {
     .get('refresh_token')
     ?.replace('refresh-token=', '') as string
 
-  console.log(new URL(window.location.href))
-  console.log(new URL(window.location.href).search)
-
-  console.log('memberId:', new URL(window.location.href).searchParams.get('memberId'))
-  console.log('access_token:', new URL(window.location.href).searchParams.get('access_token'))
-  console.log('refresh_token:', new URL(window.location.href).searchParams.get('refresh_token'))
-
   useEffect(() => {
     const socialLogin = async () => {
       dispatch(SET_TOKEN(accessToken))
@@ -32,23 +25,18 @@ const SocialLoginRedirect = () => {
         const response = await getUserInfo()
         if (response) {
           dispatch(SET_INFO(response?.data))
-          console.log(response)
         }
       } catch (err) {
         console.log(err)
       }
-      window.onpopstate = () => {
-        console.log('실행이 되고 잇나?')
-        window.history.go(-3)
-      }
-      console.log('로그인함?', new Date())
+      console.log('소셜 로그인 성공', new Date())
       return navigate('/', { replace: true })
     }
     socialLogin()
     //navigate('/', { replace: true })
   })
 
-  return <div>로그인 중...</div>
+  return <></>
 }
 
 export default SocialLoginRedirect
