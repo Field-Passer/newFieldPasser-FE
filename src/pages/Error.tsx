8a147b5696a5b9fc10135673e12b2bb587ac0ae5
iziz9 <46iz.us2@gmail.com>
2023-09-18 22:59:18 +0900

    Design: 에러페이지 아이콘 추가

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,5 +1,59 @@
+import { useNavigate } from 'react-router-dom'
+import { styled } from 'styled-components'
+import { COLORS } from '@src/globalStyles'
+import { NoticeIcon } from '@src/constants/icons'
+
 const Error = () => {
-  return <>404 NOT FOUND</>
+  const navigate = useNavigate()
+  return (
+    <Main>
+      <div onClick={() => navigate('/', { replace: true })}>
+        <img src="/logo.png" alt="logo" />
+      </div>
+      <div className="notice">
+        <NoticeIcon />
+        <span>존재하지 않는 페이지입니다.</span>
+      </div>
+      <div>
+        <button onClick={() => navigate('/', { replace: true })}>메인으로 돌아가기</button>
+      </div>
+    </Main>
+  )
 }
+
+const Main = styled.main`
+  text-align: center;
+  margin-top: 20px;
+  font-size: 26px;
+
+  img {
+    width: 200px;
+    margin-bottom: 50px;
+    cursor: pointer;
+  }
+
+  .notice {
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+
+    svg {
+      margin: 20px auto;
+    }
+  }
+
+  button {
+    background-color: ${COLORS.green};
+    padding: 10px 20px;
+    margin-top: 70px;
+    color: white;
+    font-size: 18px;
+    box-shadow: 5px 5px 5px ${COLORS.gray20};
+
+    &:hover {
+      background-color: ${COLORS.gray40};
+    }
+  }
+`
 
 export default Error
