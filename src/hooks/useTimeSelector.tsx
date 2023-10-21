b7c7dc7f5bcad29b0190c1a07c0d05ea79ab8480
iziz9 <46iz.us2@gmail.com>
2023-10-22 03:20:57 +0900

    Fix: useTimeSelector 에러로 임시 주석처리

--- src/hooks/useTimeSelector.tsx
+++ src/hooks/useTimeSelector.tsx
@@ -1,25 +1,25 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
-const useTimeSelector = ({ isTimeChange, setIsTimeChange, setSelectedTime }: ITimeSelectorProps) => {
-  const [timeSelectorOpen, setTimeSelectorOpen] = useState<boolean>()
-  const [timeZone, setTimeZone] = useState<string>('오전')
-  const [hour, setHour] = useState<string>('--')
-  const [minute, setMinute] = useState<string>('--')
+// const useTimeSelector = ({ isTimeChange, setIsTimeChange, setSelectedTime }: ITimeSelectorProps) => {
+//   const [timeSelectorOpen, setTimeSelectorOpen] = useState<boolean>()
+//   const [timeZone, setTimeZone] = useState<string>('오전')
+//   const [hour, setHour] = useState<string>('--')
+//   const [minute, setMinute] = useState<string>('--')
 
-  useEffect(() => {
-    if (hour !== '--' && minute !== '--') {
-      setTimeSelectorOpen(false)
-      setIsTimeChange(true)
+//   useEffect(() => {
+//     if (hour !== '--' && minute !== '--') {
+//       setTimeSelectorOpen(false)
+//       setIsTimeChange(true)
 
-      timeZone === '오후' && hour !== '12' && +hour + 12 <= 23
-        ? setSelectedTime(+hour + 12 + ':' + minute)
-        : setSelectedTime(hour + ':' + minute)
+//       timeZone === '오후' && hour !== '12' && +hour + 12 <= 23
+//         ? setSelectedTime(+hour + 12 + ':' + minute)
+//         : setSelectedTime(hour + ':' + minute)
 
-      timeZone === '오전' && hour === '12' && setSelectedTime('00:' + minute)
-    }
-  }, [timeZone, hour, minute])
+//       timeZone === '오전' && hour === '12' && setSelectedTime('00:' + minute)
+//     }
+//   }, [timeZone, hour, minute])
 
-  return { timeSelectorOpen, timeZone, setTimeZone, hour, setHour, minute, setMinute }
-}
+//   return { timeSelectorOpen, timeZone, setTimeZone, hour, setHour, minute, setMinute }
+// }
 
-export default useTimeSelector
+// export default useTimeSelector
