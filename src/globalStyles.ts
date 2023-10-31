a3cecb2618e964c282b8e64b6ee99741a925f18a
iziz9 <46iz.us2@gmail.com>
2023-10-31 18:56:56 +0900

    Docs: README.md 템플릿 수정

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -7,12 +7,14 @@ const GlobalStyles = createGlobalStyle`
       format('woff2');
     font-weight: normal;
     font-style: normal;
+    font-display: swap;
   }
   @font-face {
     font-family: 'Pretendard-Regular';
     src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
     font-weight: 400;
     font-style: normal;
+    font-display: swap;
   }
 
   html, body, div, span, applet, object, iframe,
